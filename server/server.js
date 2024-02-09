249c2bc7bd55c8edf4ce6cea56b74991ccad8860
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-02-09 22:53:37 +0100

    changes

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ const chargeCard = async () => {
                 "fields": [
                     "pin"
                 ],
-                "pin": 8968
+                "pin": ""
             }
             const reCallCharge = await flw.Charge.card(payload2)
 
